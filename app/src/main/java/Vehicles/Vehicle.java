5dc312a71702c536e9458bf42a9a07a7e930979d
hao.chen8 <hc222ig@student.lnu.se>
2025-10-08 17:55:13 +0200

    Add Vehicle implementation

---
+++ app/src/main/java/Vehicles/Vehicle.java
@@ -0,0 +1,51 @@
+package Vehicles;
+
+import java.util.regex.Pattern;
+
+public class Vehicle {
+  private static final Pattern VALID_PLATE = Pattern.compile("^[A-ZÅÄÖ]{3}[0-9]{2}[A-Z0-9]$|^[A-ZÅÄÖ]{3}[0-9]{3}$");
+
+  private final String regNumber;
+  private final String meta;
+
+  public Vehicle(String regNumber) {
+    this(regNumber, null);
+  }
+
+  public Vehicle(String regNumber, String meta) {
+    if (regNumber == null) throw new NullPointerException("regNumber can not be null");
+    String  norm = regNumber.trim().toUpperCase();
+
+    if (!VALID_PLATE.matcher(norm).matches()) {
+      throw new IllegalArgumentException("Invalid registration format: \" + regNumber");
+    }
+    this.regNumber = norm;
+    this.meta = meta;
+  }
+
+  public String regNumber() {
+    return regNumber;
+  }
+
+  public String meta() {
+    return meta;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Vehicle)) return false;
+    Vehicle that = (Vehicle) o;
+    return regNumber.equals(that.regNumber);
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 17;
+    for (int i = 0; i < regNumber.length(); i++) {
+      hash = 31 * hash + regNumber.charAt(i);
+    }
+    return hash;
+  }
+
+}
